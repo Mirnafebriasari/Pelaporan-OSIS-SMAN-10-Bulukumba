f550c86331060f2a5d0ef0d6b776ebd349e33ec1
Mirnafebriasari <mirnhafebriasari@gmail.com>
2024-11-07 12:36:18 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Pelaporan OSIS',
+    name='Pelaporan_OSIS',
     version='0.1.0',
     author='Mirna',
     author_email='email@mirnhafebriasari.com',
     description='Paket Python untuk pengolahan gambar',
     long_description=open('README.md').read(),  # Membaca isi README.md untuk deskripsi yang lebih panjang
     long_description_content_type='text/markdown',  # Tipe konten README
-    url='https://github.com/Mirnafebriasari/Pelaporan-OSIS.git',
+    url='https://github.com/Mirnafebriasari/Pelaporan-OSIS-SMAN-10-Bulukumba.git',
     license='MIT',
     packages=find_packages(),  # Menemukan semua paket yang ada
     install_requires=[
