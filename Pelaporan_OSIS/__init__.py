68a34eb26b466ab10b4d46127848731e87ef5108
Mirnafebriasari <mirnhafebriasari@gmail.com>
2024-11-07 18:13:26 +0700

    Update __init__.py

--- Pelaporan_OSIS/__init__.py
+++ Pelaporan_OSIS/__init__.py
@@ -2,3 +2,4 @@ from .Menambah_Laporan import Menambah_Laporan
 from .Menampilkan_Daftar_Laporan import Menampilkan_Daftar_Laporan
 from .Menghapus_Laporan import Menghapus_Laporan
 from .Mengupdate_Laporan import Mengupdate_Laporan
+from .Dokumentasi import Dokumentasi
